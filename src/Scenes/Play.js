92e1e7faf3987a8ed24c3534e13099ac82841b5e
Nathan Pon <nathanpon98@gmail.com>
2021-04-11 02:23:37 -0700

    Explosion Animation

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
      this.load.image('spaceship','./assets/spaceship.png');
      this.load.image('starfield','./assets/starfield.png');
     
+     this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});   
     }
 
    create () { 
@@ -31,9 +32,14 @@ class Play extends Phaser.Scene {
     keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
- 
+    this.anims.create({
+      key: 'explode',
+      frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+      frameRate: 30
+  });
   }
-     
+ 
+  
   update () {
       this.starfield.tilePositionX -= starSpeed;
       this.p1Rocket.update();
@@ -43,18 +49,16 @@ class Play extends Phaser.Scene {
     
       if(this.detectCollision(this.p1Rocket, this.ship03)) {
         this.p1Rocket.reset();
-        this.ship03.reset();
+        this.shipExplode(this.ship03);   
        
       }
       if (this.detectCollision(this.p1Rocket, this.ship02)) {
         this.p1Rocket.reset();
-        this.ship02.reset();
-        
+        this.shipExplode(this.ship02);   
       }
       if (this.detectCollision(this.p1Rocket, this.ship01)) {
         this.p1Rocket.reset();
-        this.ship01.reset();
-       
+        this.shipExplode(this.ship01);  
       }
 
   }
@@ -69,6 +73,17 @@ class Play extends Phaser.Scene {
     else {
         return false;
     }
+  }
+  shipExplode (ship) {
+    ship.alpha = 0;
+
+    let explode = this.add.sprite (ship.x, ship.y, 'explosion').setOrigin(0,0);
+    explode.anims.play ('explode')
+    explode.on ('animationcomplete', () => {
+     ship.reset();                         // reset ship position
+     ship.alpha = 1;                       // make ship visible again
+     explode.destroy();                       // remove explosion sprite
+    });
   }
    }
 
