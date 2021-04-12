c049377d4a86c7dc21a4c2062fed16c329b647ea
Nathan Pon <nathanpon98@gmail.com>
2021-04-11 19:15:21 -0700

    Menu Configuration

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -2,8 +2,30 @@ class Menu extends Phaser.Scene {
  constructor() {
      super ("menuScene");
  }   
- create () { this.add.text(20,20, "Rocket Patrol Menu");
-
- this.scene.start("playScene");
-}
+ preload () {
+ //load audio
+    this.load.audio ('sfx_select', './assets/blip_select12.wav')
+    this.load.audio ('sfx_explosion', './assets/explosion38.wav')
+    this.load.audio ('sfx_rocket', './assets/rocket_shot.wav')
+ }
+ create () { 
+    let menuConfig = {
+    fontFamily: 'Courier',
+    fontSize: '28px',
+    backgroundColor: '#F3B141',
+    color: '#843605',
+    align: 'right',
+    padding: {
+      top: 5,
+      bottom: 5,
+    },
+    fixedWidth: 0
+ }
+//Menu Screen
+this.add.text(game.config.width/2, game.config.height/2- borderUIsize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0,5);
+this.add.text(game.config.width/2, game.config.height/2, "Use Arrows to move and (F) to Fire", menuConfig).setOrigin.setOrigin(0,5);
+menuConfig,backgroundColor = '#00FF00';
+menuConfig.color = '#000';
+his.add.text(game.config.width/2, game.config.height/2 + borderUIsize + borderPadding, 'Press <- for Novice and -> for Expert', menuConfig).setOrigin(0,5);
+  }
 }
