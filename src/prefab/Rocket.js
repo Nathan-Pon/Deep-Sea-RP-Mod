e271cf067939908911a8787ea59b1de8ff011938
Nathan Pon <nathanpon98@gmail.com>
2021-04-17 23:38:46 -0700

    moveSpeed updated

--- src/prefab/Rocket.js
+++ src/prefab/Rocket.js
@@ -3,7 +3,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene,x, y,texture, frame);
         scene.add.existing(this); //Place Rocket in scene
         this.isFiring = false;
-        this.moveSpeed = 6;
+        this.moveSpeed = 2;
         this.sfxRocket = scene.sound.add('sfx_rocket');
     }
     update() {
