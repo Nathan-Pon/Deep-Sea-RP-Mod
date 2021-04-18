78eddb95120305ca0f40c4e19ede31cb4a3f3a51
Nathan Pon <nathanpon98@gmail.com>
2021-04-17 20:38:12 -0700

    new Enemy

---
+++ src/prefab/jellyFish.js
@@ -0,0 +1,20 @@
+class jellyFish extends Phaser.GameObjects.Sprite {
+    constructor (scene, x, y, texture, frame, pointValue) {
+     super (scene,x,y, texture, frame);
+     scene.add.existing(this);
+     this.points = pointValue;
+     this.moveSpeed =3;
+   
+    }
+    update() {
+        //move spaceship left
+        this.x += this.moveSpeed;
+       //Wrap around the screen
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+    reset() {
+        this.x = game.config.width;
+    }
+   } 
