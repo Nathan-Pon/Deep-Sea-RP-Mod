5c86e63017830d61fc6e4688485547666bbd5e97
Nathan Pon <nathanpon98@gmail.com>
2021-04-21 01:48:58 -0700

    Increased jellyFish Point Value

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 /*
 Nathan Pon
+4/19/2021
  CMPM 120
  RP Mod - Harpoon of the Deep
 Estimated time: 13 hours
- Points Breakdown
+
+Points Breakdown
  60 (Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)) 
  20 (Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points)
   5 (Allow the player to control the Rocket after it's fired)
